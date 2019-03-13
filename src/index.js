bbd86b7ebf1e08c663bccd904680d374a40716dc
Forkossigan <andrebakalov@mail.ru>
2019-03-14 00:35:56 +0300

    fix: fixed code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
+
   var n = preferences.length;
   var j;
   var z;
@@ -19,5 +20,5 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
     j++ 
     
   };
-  return s/3;
+  return s;
 };
