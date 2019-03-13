66c889474211dc9bce040f6ff21f9f561faaba2e
Forkossigan <andrebakalov@mail.ru>
2019-03-14 01:33:15 +0300

    fix: fixed code

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-  let length = preferences.length; 
-  let count = 0;  
-  for (let i = 0; i<length; i++) { 
-      let next = preferences[i] - 1;
-      if (next == i) continue;
-      let nextnext = preferences[next] - 1;
-      if (next == nextnext) continue;
-      let nextnextnext = preferences[nextnext] - 1;
-      if (nextnextnext == nextnext) continue;
-      if (nextnextnext == i) {
-          count++;
-      }
-   }
-   return count / 3;
+module.exports  = function getLoveTrianglesCount(preferences = []) {
+ var n = preferences.length;
+var j ;
+var z;
+var r;
+var m;
+var s = 0;
+for (j = 0; j<n; j++) {
+ 
+  z = preferences[j] - 1;
+  r = preferences[z] - 1;
+  m = preferences[r] - 1;
+  if ( j == m){
+    s++
+  
+  }
+   
+
+}
+return s;
 }
