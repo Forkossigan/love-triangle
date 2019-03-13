c8e4f11a572c2119df4d9fabc2b764bbd1b3390e
Forkossigan <andrebakalov@mail.ru>
2019-03-14 01:41:52 +0300

    fix: fixed code

--- src/index.js
+++ src/index.js
@@ -12,9 +12,12 @@ var s = 0;
 for (j = 0; j<n; j++) {
  
   z = preferences[j] - 1;
+  if (z == i) continue;
   r = preferences[z] - 1;
+  if (r == z) continue;
   m = preferences[r] - 1;
-  if ( j == m){
+  if (m == r) continue;
+  if ( j + 1 == m){
     s++
   
   }
